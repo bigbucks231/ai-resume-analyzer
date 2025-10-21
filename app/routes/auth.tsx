47159b26ba188ac7b57be3011a8c38762a88d829
Abdul <beshielbah@icloud.com>
2025-10-21 23:20:24 +0000

    Auth With Puter Done

---
+++ app/routes/auth.tsx
@@ -0,0 +1,18 @@
+export const meta = () => ([
+    { title: 'Resumind | Auth' },
+    { name: 'description', content: 'Log into your account' }
+])
+
+const Auth = () => {
+    return (
+        <main className='bg-[url("/images/bg-auth.svg")] bg-cover min-h-screen flex items-center justify-center'>
+            <div className='gradient-border shadow-lg'>
+                <section className='flex flex-col gap-8 rounded-2xl bg-white p-10'>
+
+                </section>
+            </div>
+        </main>
+    );
+};
+
+export default Auth;
